71e3c439dd21607c5872cdfe1eb2ba0fe6d13c70
Shridhar-Atram <shridharatram555@gmail.com>
2023-12-14 20:44:20 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
+// App.js
+
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { format } from 'date-fns';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [query, setQuery] = useState('');
+  const [category, setCategory] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchArticles = async () => {
+      try {
+        const res = await fetch(`https://hn.algolia.com/api/v1/search?query=${query}`);
+        const data = await res.json();
+        setItems(data.hits);
+        setIsLoading(false); // Set loading to false when data is fetched
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setIsLoading(false); // Set loading to false in case of an error
+      }
+    };
+
+    fetchArticles();
+    setCategory(query); // Update the category based on the current query
+  }, [query]); // Add query as a dependency to the useEffect hook
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section">
+      <form autoComplete="off" onSubmit={(e) => e.preventDefault()}>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search for something"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit" onClick={() => setQuery(query)}>
+          Search
+        </button>
+      </form>
+      {isLoading ? (
+        <div className="spinner"></div>
+      ) : (
+        <div>
+          {category && <h2>Category: {category}</h2>}
+          <div className="cards">
+            {items.map(({ author, created_at, title, url, objectID }) => (
+              <div key={objectID}>
+                <h2>{title}</h2>
+                <ul>
+                  <li> By {author}</li>
+                  <li>
+                    <a href={url} target="_blank" rel="noreferrer">
+                      Read Full Article
+                    </a>
+                  </li>
+                </ul>
+                <p>{created_at}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </section>
   );
 }
 
